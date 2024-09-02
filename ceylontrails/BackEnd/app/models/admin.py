880a5d1f4b79f2767976185694499502277e9f09
Sasanka Ravindu <61648299+SasankaRW@users.noreply.github.com>
2024-09-03 00:15:47 +0530

    controller added

--- ceylontrails/BackEnd/app/models/admin.py
+++ ceylontrails/BackEnd/app/models/admin.py
@@ -3,20 +3,8 @@ from typing import Optional
 from datetime import datetime
 from bson import ObjectId
 
-class PyObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, v):
-        if not ObjectId.is_valid(v):
-            raise ValueError('Invalid objectid')
-        return ObjectId(v)
-
-    @classmethod
-    def __modify_schema__(cls, field_schema):
-        field_schema.update(type="string")
+def objectid_to_str(object_id: ObjectId) -> str:
+    return str(object_id)
 
 class AdminModel(BaseModel):
     _id: Optional[PyObjectId] = Field(default_factory=PyObjectId, alias="_id")
@@ -43,3 +31,4 @@ class AdminModel(BaseModel):
                 "updated_at": "2024-09-02T00:00:00Z"
             }
         }
+
